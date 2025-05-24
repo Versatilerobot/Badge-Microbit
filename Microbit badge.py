eb8522734f89f5941c2bc94b69871e8e7724ff65
Versatilerobot <158510161+Versatilerobot@users.noreply.github.com>
2025-05-24 14:16:14 +0200

    Add files via upload

--- Microbit badge.py
+++ Microbit badge.py
@@ -4,9 +4,17 @@ import audio
 import utime
 import music
 
+try:
+    speaker.on()
+except NameError:
+    speaker = None
+
+try:
+    from microbit import Sound
+except ImportError:
+    Sound = None
+
 start = utime.ticks_ms()
-tune = ["C4:4", "D4:4", "E4:4", "C4:4", "C4:4", "D4:4", "E4:4", "C4:4",
-        "E4:4", "F4:4", "G4:8", "E4:4", "F4:4", "G4:8"]
 
 while True:
 
